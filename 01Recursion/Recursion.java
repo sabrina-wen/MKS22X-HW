de64f5e772b39818fc16c05ed062978a31960037
sabrina-wen <sabrinawn253@gmail.com>
2017-02-07 23:32:17 -0500

    fixed sqrt and helper methods w/ comments and test cases

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,4 +1,5 @@
 public class Recursion {
+    public static double better_guess;
     
     public static String name() {
 	return "Wen,Sabrina";
@@ -8,15 +9,31 @@ public class Recursion {
 	if (n < 0) {
 	    throw new IllegalArgumentException();
 	}
-	return helper();
+
+	return helper(n, 1.0);
     }
     
     public static double helper(double number, double guess) {
-	// base case
-	
+	/// base case since n >= 0.0
+	if (number == 0.0) {
+	    return 0;
+	}
+
+	// formula for better guess, return the sqrt
+	better_guess = (number / guess + guess) / 2.0;
+
+	//checking to see if better_guess and guess are close enough
+	if ((Math.abs(number - better_guess * better_guess)) < 0.0000001) {
+	    return better_guess;
+	}
+
+	// recursive part of the fn
+	return helper(number, better_guess);
     }
 
     public static void main (String[] args) {
-	System.out.println(sqrt(25));f
+	System.out.println(sqrt(36.0));
+       	System.out.println(sqrt(11.0));
+   	System.out.println(sqrt(9.0));
     }
 }
