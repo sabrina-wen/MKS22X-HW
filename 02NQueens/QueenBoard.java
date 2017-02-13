8cd8ac134a0bb29ab515d9da451de75c1c43832a
sabrina-wen <sabrinawn253@gmail.com>
2017-02-13 16:52:36 -0500

    added addqueen and replacequeen, and solveh, but not really working correctly

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -21,17 +21,64 @@ public class QueenBoard {
     }
 
     private boolean solveH(int col){
-        
+	int i = 0;
+        for (i = 0; i < board.length; i++) {
+	    if (addQueen(i, col)) {
+		if (col == board.length - 1) {
+		    return true;
+		}
+	        if (solveH(col + i)) {
+		    return true;
+		}
+		else {
+		    removeQueen(i, col);
+		}
+	    }
+	}
 	return false;
     }
 
-    private int addQueen(int row, int col) {
-	if (isPlaceable) {
-	    for (int r = 0; r < board.length; r++) {
-		checkIfQueenIsAlreadyInRow - compare xs;
+    // if a cell is occupied by a queen, cell == -1
+    // if cell is not occupied by a queen and can't be occupied, it will be a # > 0
+    // if a cell isn't occupied but could be occupied, cell == 0
+    private boolean addQueen(int row, int col) {
+	if (isPlaceable(row, col)) {
+	    board[row][col] -= 1;
+	    // adding 1 to all other cells in rows
+	    for (int i = col + 1; i < board[0].length; i++) {
+		board[row][i] += 1;
 	    }
+	    for (int j = row - 1; j >= 0; j--) {
+		board[j][col] += 1;	   
+	    }
+	    for (int k = col + 1; k < board[0].length; k++) {
+	      	for (int l = row - 1; l >= 0; l--) {
+		    board[k][l] += 1;
+                }
+	    }	    
 	    return true;
-	}
+	} 
+        return false;
+    }
+
+    private boolean removeQueen(int row, int col) {
+	if (!isPlaceable(row, col)) {
+	    board[row][col] += 1;
+	    // subtracting 1 to all other cells in rows
+	    for (int i = col + 1; i < board[0].length; i++) {
+		board[row][i] -= 1;
+	    }
+	    for (int j = row - 1; j >= 0; j--) {
+		board[j][col] -= 1;	   
+	    }
+	    for (int k = col + 1; k < board[0].length; k++) {
+	      	for (int l = row - 1; l >= 0; l--) {
+		    board[k][l] -= 1;
+                }
+	    }	    
+	    return true;
+	} 
+        return false;
     }
 
     private boolean isPlaceable(int row, int col) {
@@ -56,9 +103,9 @@ public class QueenBoard {
         String solution = "";
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[0].length; j++) {
-		solution += board[i][j] + "_";
+		solution += board[i][j] + "  ";
 	    }
-	    solution += "_";
+	    solution += "\n";
 	}
 	return solution;
     }
@@ -67,7 +114,9 @@ public class QueenBoard {
     // }
 
     public static void main (String[] args) {
-	QueenBoard b = new QueenBoard(2);
+	QueenBoard b = new QueenBoard(4);
+	// b.addQueen(0,0);
+	b.solve();
 	System.out.println(b.toString());
     }
 }
