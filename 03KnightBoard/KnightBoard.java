260d56bca9888913168f813906b071914a25a14a
sabrina-wen <sabrinawn253@gmail.com>
2017-02-17 15:32:53 -0500

    initial file from site

---
+++ 03KnightBoard/KnightBoard.java
@@ -0,0 +1,13 @@
+public class KnightBoard {
+    public KnightBoard (int startingRows, int startingCols) {
+    }
+
+    public String toString() {
+    }
+
+    public void solve() {
+    }
+
+    private boolean solveH (int row, int col, int level) {
+    }
+}
