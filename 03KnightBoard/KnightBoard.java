4e326c63cbd9fa27255da4c13cdfd0a9f8cae995
sabrina-wen <sabrinawn253@gmail.com>
2017-02-23 17:37:05 -0500

    working on solveH, returns an error rn

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -19,16 +19,50 @@ public class KnightBoard {
 	return solution;
     }
 
-    // public void solve() {
-        
-    // }
+    public void solve() {
+        solveH(0,0,1);
+    }
 
-    // private boolean solveH (int row, int col, int level) {
-    // }
+    /**  set knight down
+	 try all possible solutions by looping thru arrays
+	 set a knight down for each possible move and check to see if another move is possible by calling fn now
+	 
+	 if not then backtrack by subtract 1 from level, set current cell = to 0, and then try next solution in the array
+	 if all solutions from the arrays have been exhausted, then you're done and board can't be solved
+     **/
+
+    private boolean solveH (int row, int col, int level) {
+	board[row][col] = level;
+	if (board.length * board[0].length == level - 1) {
+	    return true;
+	}
+	else {
+	    int[] colOptions = {-1, 1, 2, 2, 1, -1, -2, -2};
+	    int[] rowOptions = {-2, -2, -1, 1, 2, 2, 1, -1};
+	    // board[row][col] = level;
+	    for (int i = 0; i < rowOptions.length; i++) {
+		for (int j = 0; j < colOptions.length; j++) {
+		    int rowOffset = rowOptions[i];
+		    int colOffset = colOptions[j];
+		    if ((row + rowOffset < board.length && row + rowOffset > -1 )
+			&& (col + colOffset < board[0].length && row + colOffset > -1)
+			&& (board[row + rowOffset][col + colOffset] == 0)) {
+			row += rowOffset;
+			col += colOffset;
+			level++;
+			board[row][col] = level;
+			solveH(row, col, level + 1);
+		    }
+		}
+	    }
+	    
+	}
+	return false;
+    }
 
     public static void main (String[] args) {
-	KnightBoard b = new KnightBoard(4, 4);
-	// b.solve();
+	KnightBoard b = new KnightBoard(7, 7);
+	b.solve();
 	System.out.println(b.toString());
     }
 }
