a954c3a39714d44a52b2f75613906bb67155c7f9
sabrina-wen <sabrinawn253@gmail.com>
2017-02-23 15:00:21 -0500

    added constructor, board variable, and toString methods which all work

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,13 +1,34 @@
 public class KnightBoard {
+    private int[][] board;
+    
     public KnightBoard (int startingRows, int startingCols) {
+	board = new int[startingRows][startingCols];
     }
 
     public String toString() {
+	String solution = "";
+	for (int r = 0; r < board.length; r++) {
+	    for (int c = 0; c < board[0].length; c++) {
+		if (board[r][c] < 10) {
+		    solution += "_" + board[r][c];
+		}
+		solution += " ";
+	    }
+	    solution += "\n";
+	}
+	return solution;
     }
 
-    public void solve() {
-    }
+    // public void solve() {
+        
+    // }
 
-    private boolean solveH (int row, int col, int level) {
+    // private boolean solveH (int row, int col, int level) {
+    // }
+
+    public static void main (String[] args) {
+	KnightBoard b = new KnightBoard(4, 4);
+	// b.solve();
+	System.out.println(b.toString());
     }
 }
