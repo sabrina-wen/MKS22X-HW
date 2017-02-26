23d42bef536f99a22859ce1f523124b165a11f05
sabrina-wen <sabrinawn253@gmail.com>
2017-02-26 02:41:08 -0500

    restructured solveH, works for all boards under 8x8 except for 7x7

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -10,7 +10,7 @@ public class KnightBoard {
 	for (int r = 0; r < board.length; r++) {
 	    for (int c = 0; c < board[0].length; c++) {
 		if (board[r][c] < 10) {
-		    solution += "_" + board[r][c];
+		    solution += " " + board[r][c];
 		}
 		else {
 		    solution += board[r][c];
@@ -35,32 +35,35 @@ public class KnightBoard {
     **/
 
     private boolean solveH (int row, int col, int level) {
-	System.out.println(this);
+	// System.out.println(this);
 	// board[row][col] = level;
-	if (board.length * board[0].length == level) {
+	if (level > board.length * board[0].length) {
 	    return true;
 	}
-	else {
-            board[row][col] = level;
+	if (board[row][col] == 0) {
+	    board[row][col] = level;
 	    int[] colOptions = {-1, 1, 2, 2, 1, -1, -2, -2};
 	    int[] rowOptions = {-2, -2, -1, 1, 2, 2, 1, -1};
 	    for (int i = 0, j = 0; i < rowOptions.length && j < colOptions.length; i++, j++) {
 		int rowOffset = rowOptions[i];
 		int colOffset = colOptions[j];
-		if ((row + rowOffset < board.length && row + rowOffset > -1 )
-		    && (col + colOffset < board[0].length && col + colOffset > -1)
-		    && (board[row + rowOffset][col + colOffset] == 0)) {
-		    solveH(row + rowOffset, col + colOffset, level + 1);
+		if ((row + rowOffset < board.length && row + rowOffset >= 0 )
+		    && (col + colOffset < board[0].length && col + colOffset >= 0)) {
+		    if (solveH(row + rowOffset, col + colOffset, level + 1)) {
+			return true;
+		    }
 		}
+		    
 	    }
 	    board[row][col] = 0;
-	    solveH(row, col, level + 1);	     
 	}
 	return false;	
     }
 
 	public static void main (String[] args) {
-	    KnightBoard b = new KnightBoard(7, 7);
+	    // works for the following:
+	    // 6x7, 7x8, 6x8, 6x6, 5x5, 4x7
+	    KnightBoard b = new KnightBoard(8,8);
 	    b.solve();
 	    System.out.println(b.toString());
 	}
