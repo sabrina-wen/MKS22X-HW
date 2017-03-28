9975b8a2b99faeb3c6fb04f3dab7eabd98283145
sabrina-wen <sabrinawn253@gmail.com>
2017-03-28 15:34:27 -0400

    started linked list

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,20 @@
+public class MylinkedList {
+    LNode start, end;
+    int size;
+
+    private class LNode {
+	int currentVal, nextVal, prevVal;
+    }
+
+    private boolean add (int value) {
+        LN
+	size++;
+    }
+
+    public String toString() {
+	String strList = "[";
+	for (int i = 0; i < size(); i++) {
+	    strList += ;
+	}
+    }
+}
