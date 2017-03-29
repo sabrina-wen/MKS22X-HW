60ac79a7c71419a74232452f350d69ae495386a7
sabrina-wen <sabrinawn253@gmail.com>
2017-03-29 00:40:42 -0400

    done done done WOOT

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -12,29 +12,66 @@ public class Merge {
 	mergeTheTwoHalvesIntoTheOriginalArray **/
 
     public static void mergesort (int[] ary) {
+	int[] left = new int[ary.length / 2];
+	int[] right = new int[ary.length - left.length];
 	if (ary.length == 0 || ary.length == 1) {
+	    // System.out.println(printAry(ary));
 	    return;
 	}
-	int[] left = new int[ary.length / 2];
-	System.out.println("Left ary length: " + left.length);
-	int[] right = new int[ary.length - left.length];
+	// int[] left = new int[ary.length / 2];
+	// System.out.println("Left ary length: " + left.length);
+	// int[] right = new int[ary.length - left.length];
 	for (int i = 0; i < ary.length / 2; i++) {
 	    left[i] = ary[i];
 	}
-	System.out.println("Left array: " + printAry(left));
+	// System.out.println("Left array: " + printAry(left));
 	for (int j = ary.length / 2; j < ary.length; j++) {
 	    right[j - left.length] = ary[j];
 	}
-	System.out.println("Right array: " + printAry(right));
-	System.out.println("Whole array: " + printAry(ary));
+	// System.out.println("Right array: " + printAry(right));
+	// System.out.println("Whole array: " + printAry(ary));
 	mergesort(left);
         mergesort(right);
-	// merge(left, right, ary);
-	
+	merge(left, right, ary);
+	// System.out.println(printAry(ary));
     }
 
-    /** public static void merge(int[]a,int[]b,int[]destination) {
-	} **/
+    public static void merge(int[]a,int[]b,int[]destination) {
+	int aCount = 0; // pos in left ary (a)
+	int bCount = 0; // pos in right ary (b)
+	int count = 0; // count = pos in ary
+
+	// ordering elements in a pair
+	while (aCount < a.length && bCount < b.length) {
+	    if (a[aCount] < b[bCount]) {
+		destination[count] = a[aCount];
+		aCount++;
+		count++;
+		// System.out.println("Count val: " + count);
+	    }
+	    else {
+		destination[count] = b[bCount];
+		bCount++;
+		count++;
+	        // System.out.println("Count val: " + count);
+	    }
+	}
+
+	// so as you go from a pair to a trio or more,
+	// u basically incrememt to copy the remaining elements
+	for (int i = aCount; i < a.length; i++) {
+	    destination[count] = a[i];
+	    count++;
+	    //  System.out.println("Count val: " + count);
+	}
+
+	for (int i = bCount; i < b.length; i++) {
+	    destination[count] = b[i];
+	    count++;
+	    //  System.out.println("Count val: " + count);
+	}
+    }
+	   
 
     public static String printAry(int[] ary) {
 	String printedAry = "";
@@ -45,9 +82,10 @@ public class Merge {
 	return printedAry;
     }
 
-    public static void main (String[] args) {
+    /** public static void main (String[] args) {
 	int[] test = {38, 27, 43, 3, 9, 82, 10};
 	int[] test1 = {-11, -29383};
         mergesort(test);
-    }
+	System.out.println(printAry(test));
+	} **/
 }
