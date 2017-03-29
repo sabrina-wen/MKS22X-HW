e5a789cb524577d486e611d3d68a41650aecdfd8
sabrina-wen <sabrinawn253@gmail.com>
2017-03-29 00:49:47 -0400

    idk what i'm doing

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,20 +1,37 @@
 public class MylinkedList {
-    LNode start, end;
-    int size;
+    private LNode start, end;
+    private int size;
 
     private class LNode {
-	int currentVal, nextVal, prevVal;
+	int currentVal;
+	LNode next;
+
+	public LNode(int value) {
+	    this.value = value;
+	    next = null;
+	}
+
+	public LNode (int value, LNode next) {
+	    this.value = value;
+	    this.next = next;
+	}
     }
 
-    private boolean add (int value) {
-        LN
+    public int size() {
+	return size;
+    }
+
+    public boolean add (int value) {
+        LNode start = new LNode(value);
 	size++;
     }
 
     public String toString() {
 	String strList = "[";
 	for (int i = 0; i < size(); i++) {
-	    strList += ;
+	    strList += 
 	}
     }
+
+    
 }
