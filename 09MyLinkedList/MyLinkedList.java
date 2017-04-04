7db04cac1a2ddf68d7dbd52f954e7821c6c130a1
sabrina-wen <sabrinawn253@gmail.com>
2017-04-04 01:54:43 -0400

    toString works, fixed constructors, add works if u just wanna add 1 element to a null list otherwise that's it lol

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,18 +1,20 @@
-public class MylinkedList {
+public class MyLinkedList {
     private LNode start, end;
     private int size;
 
+    public MyLinkedList() {
+	start = null;
+	end = null;
+	size = 0;
+    }
+
     private class LNode {
-	int currentVal;
-	LNode next;
+	private int currentVal;
+	private LNode next;
 
-	public LNode(int value) {
-	    this.value = value;
-	    next = null;
-	}
-
+	// note: if it's the last element then next = null
 	public LNode (int value, LNode next) {
-	    this.value = value;
+	    currentVal = value;
 	    this.next = next;
 	}
     }
@@ -21,17 +23,37 @@ public class MylinkedList {
 	return size;
     }
 
+    // for now i'm adding an element to the beginning of the list
     public boolean add (int value) {
-        LNode start = new LNode(value);
+	if (size > 0) {
+	    LNode addedVal = new LNode(value, start);
+	}
+	if (size == 0) {
+	    start = new LNode(value, null);
+	}
 	size++;
+	return true;
+    }
+
+    public void add (int index, int value) {
+	// idk
     }
 
     public String toString() {
 	String strList = "[";
-	for (int i = 0; i < size(); i++) {
-	    strList += 
+	LNode current = start;
+        while (current != null) {
+	    strList += current.currentVal + ", ";
+	    current = current.next;
 	}
+	strList += "]";
+	return strList;
     }
 
-    
+    public static void main (String[] args) {
+	MyLinkedList list1 = new MyLinkedList();
+	list1.add(11);
+       	list1.add(541);
+	System.out.println(list1.toString());
+    }
 }
