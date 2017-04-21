2c4190066cdc5a911135eaa1b3b13a70b031cc8e
sabrina-wen <sabrinawn253@gmail.com>
2017-04-21 12:26:39 -0400

    starting deque

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,11 @@
+public class MyDeque extends Queue<String> {
+    private String[] ary;
+
+    public void addFirst(String s) {
+    }
+
+    public String toString() {
+	String retVal = "";
+	
+    }
+}
