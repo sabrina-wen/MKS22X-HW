5f417057e9340781fb3ab05c0dad942658ee8009
sabrina-wen <sabrinawn253@gmail.com>
2017-05-05 01:45:21 -0400

    added wrong versin of myheap from school before

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -2,7 +2,7 @@ public class MyHeap {
     private String[] ary;
     private int size;
 
-    public myHeap() {
+    public MyHeap() {
 	ary = new String[10];
 	size = 0;
     }
@@ -20,9 +20,38 @@ public class MyHeap {
 
     public String toString() {
 	String ret = "[";
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < ary.length; i++) {
 	    ret += ary[i] + ", ";
 	}
+	ret += "]";
+	return ret;
     }
 
+    public void add(String s) {
+        ary[size + 1] = s;
+	size++;
+    }
+
+    public void pushUp() {
+    }
+
+    public void pushDown() {
+    }
+
+    public int setPos(String s) {
+	int pos = -1;
+	int compVal;
+	for (String str:ary) {
+	    int val = s.compareTo(ary[i]);
+	    if (val < compVal)
+    }
+
+    public static void main (String[] args) {
+	MyHeap one = new MyHeap();
+	one.add("a");
+	one.add("b");
+	one.add("d");
+	System.out.println(one);
+	// System.out.println(one.size);
+    }
 }
