9eec0b31e2d5efa64eac9685d7f2d87dfbc52c92
sabrina-wen <sabrinawn253@gmail.com>
2017-05-09 01:37:04 -0400

    done

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,14 +1,24 @@
 public class MyHeap {
     private String[] ary;
     private int size;
+    private int changer;
 
     public MyHeap() {
 	ary = new String[10];
 	size = 0;
+	changer = -1;
     }
 
-    /** public myHeap(boolean isMax) {
-	} **/
+    public MyHeap(boolean isMax) {
+	ary = new String[10];
+	size = 0;
+	if (isMax) {
+	    changer = -1;
+	}
+	else {
+	    changer = 1;
+	}
+    } 
 
     public void resize() {
         String[] newAry = new String[size * 2];
@@ -27,6 +37,7 @@ public class MyHeap {
 	return ret;
     }
 
+    // this is for a min heap
     public void add(String s) {
         if (size == ary.length - 1) {
 	    resize();
@@ -40,7 +51,7 @@ public class MyHeap {
     private void pushUp(int pos) {
 	while (pos >= 2) {
 	    int parent = pos / 2;
-	    if (ary[parent].compareTo(ary[pos]) > 0) {
+	    if (changer * ary[parent].compareTo(ary[pos]) > 0) {
 		swap(parent, pos);
 	    }
 	    pos--;
@@ -53,20 +64,48 @@ public class MyHeap {
 	ary[b] = temp;
     }
 
-    /** public String remove(String s) {
+    public String remove() { // always removes top node
+	String retVal = ary[1];
+	ary[1] = ary[size];
+	ary[size] = null;
+	size--;
+	pushDown();
+	return retVal;
     }
 
     private void pushDown() {
-    } **/
+	int index = 1;
+	while (index < size && ary[index * 2] != null && ary[index * 2 + 1] != null) {
+	    if (changer * ary[index].compareTo(ary[index * 2]) > 0) {
+		swap(index, index * 2);
+	    }
+	    if (changer * ary[index].compareTo(ary[index * 2 + 1]) > 0) {
+		swap(index, index * 2 + 1);
+	    }
+	    index++;
+	}
+    }
 
-    public static void main (String[] args) {
-	MyHeap one = new MyHeap();
+    public String peek() {
+	return ary[1];
+    }
+
+    /** public static void main (String[] args) {
+	System.out.println("z".compareTo("a"));
+	MyHeap one = new MyHeap(false);
 	one.add("e");
-        one.add("b");
-	one.add("d");
-	one.add("a");
+        one.add("a");
+	one.add("g");
+	one.add("z");
+	one.add("cy");
+	one.add("r");
+	one.add("c");
+	one.add("b");
 	System.out.println(one);
+	one.remove();
+	System.out.println(one);
+	one.peek();
 	// System.out.println("b".compareTo("c"));
 	// System.out.println(one.size);
-    }
+	} **/
 }
