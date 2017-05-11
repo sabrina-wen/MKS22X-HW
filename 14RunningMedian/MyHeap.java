9e71096346d3b6d7171cc272e62e8bde3e77382f
sabrina-wen <sabrinawn253@gmail.com>
2017-05-10 23:43:41 -0400

    made some changes but idk why remove works fine here but not w/ runningmedian

--- 14RunningMedian/MyHeap.java
+++ 14RunningMedian/MyHeap.java
@@ -1,16 +1,16 @@
 public class MyHeap {
-    private int[] ary;
+    private Integer[] ary;
     private int size;
     private int changer;
 
     public MyHeap() {
-	ary = new int[1];
+	ary = new Integer[1];
 	size = 0;
 	changer = -1;
     }
 
     public MyHeap(boolean isMax) {
-	ary = new int[10];
+	ary = new Integer[10];
 	size = 0;
 	if (isMax) {
 	    changer = -1;
@@ -21,7 +21,7 @@ public class MyHeap {
     } 
 
     public void resize() {
-        int[] newAry = new int[size * 2];
+        Integer[] newAry = new Integer[size * 2];
 	for (int i = 0; i < ary.length; i++) {
 	    newAry[i] = ary[i];
 	}
@@ -51,7 +51,7 @@ public class MyHeap {
     private void pushUp(int pos) {
 	while (pos >= 2) {
 	    int parent = pos / 2;
-	    if (changer * ary[parent] > ary[pos]) {
+	    if (changer * ary[parent].compareTo(ary[pos]) > 0) {
 		swap(parent, pos);
 	    }
 	    pos--;
@@ -66,20 +66,21 @@ public class MyHeap {
 
     public int remove() { // always removes top node
 	int retVal = ary[1];
-	ary[1] = ary[size];
-	ary[size] = 0;
+	swap(ary[1], ary[size]);
+	ary[size] = null;
 	size--;
 	pushDown();
+	// System.out.println("removed: " + retVal);
 	return retVal;
     }
 
     private void pushDown() {
 	int index = 1;
-	while (index <= size / 2) {
-	    if (changer * ary[index] > ary[index * 2]) {
+	while (index * 2 + 1 < size) {
+	    if (changer * (Integer)ary[index].compareTo((Integer)ary[index * 2]) < 0) {
 		swap(index, index * 2);
 	    }
-	    if (changer * ary[index] > ary[index * 2 + 1]) {
+	    if (changer * (Integer)ary[index].compareTo((Integer)ary[index * 2 + 1]) < 0) {
 		swap(index, index * 2 + 1);
 	    }
 	    index++;
@@ -94,9 +95,10 @@ public class MyHeap {
 	return size;
     }
 
-    /** public static void main (String[] args) {
+     public static void main (String[] args) {
 	MyHeap one = new MyHeap(false);
-	one.add(11);
+	// System.out.println(((Integer)2).compareTo((Integer)3));
+	/** one.add(11);
         one.add(3);
 	one.add(-5);
 	one.add(6);
@@ -112,8 +114,13 @@ public class MyHeap {
 	one.remove();
 	System.out.println(one);
 	System.out.println(one.size);
-	one.peek();
+	one.peek(); **/
+        for (int i = 1; i <= 4; i++) {
+	    one.add(i);
+	}
+	one.remove();
+	System.out.println(one);
 	// System.out.println("b".compareTo("c"));
 	// System.out.println(one.size);
-	} **/
+	}  
 }
