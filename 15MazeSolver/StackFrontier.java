5c21c35b3660c6b0bc2c275ec96c1c382eda9c14
sabrina-wen <sabrinawn253@gmail.com>
2017-05-22 00:50:27 -0400

    did not test this yet but i think it will work

--- 15MazeSolver/StackFrontier.java
+++ 15MazeSolver/StackFrontier.java
@@ -1,5 +1,20 @@
-import java.util.*;
+import java.util.Stack;
 
-public class StackFrontier implements Frontier { // have not yet written frontier lol
+public class StackFrontier implements Frontier { 
     private Stack<Location> stackLoc;
+
+    public StackFrontier() {
+	stackLoc = new Stack<Location>();
+
+    public void add (Location loc) {
+	stackLoc.add(loc);
+    }
+
+    public Location next() {
+	return stackLoc.pop();
+    }
+
+    public int size() {
+	return stackLoc.size();
+    }
 }
