eaf066c09b4614c74781246e2a72778ca610eefd
sabrina-wen <sabrinawn253@gmail.com>
2017-05-22 01:55:36 -0400

    good

--- 15MazeSolver/StackFrontier.java
+++ 15MazeSolver/StackFrontier.java
@@ -2,19 +2,30 @@ import java.util.Stack;
 
 public class StackFrontier implements Frontier { 
     private Stack<Location> stackLoc;
+    private int size;
 
     public StackFrontier() {
 	stackLoc = new Stack<Location>();
+    }
 
     public void add (Location loc) {
-	stackLoc.add(loc);
+	stackLoc.push(loc);
+	size++;
     }
 
     public Location next() {
+	size--;
 	return stackLoc.pop();
     }
 
     public int size() {
 	return stackLoc.size();
     }
+
+    public static void main (String[] args) {
+	StackFrontier s = new StackFrontier();
+	s.add(new Location(2, 3, null, 0, 0, false));
+	s.next();
+    }
+
 }
