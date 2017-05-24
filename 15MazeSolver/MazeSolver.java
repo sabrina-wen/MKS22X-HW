42bd659e511ecc6dacd19a608cbe2db9d442b673
sabrina-wen <sabrinawn253@gmail.com>
2017-05-24 01:38:07 -0400

    did constructors and started solve method, will finish tomorrow i swear

--- 15MazeSolver/MazeSolver.java
+++ 15MazeSolver/MazeSolver.java
@@ -1,8 +1,32 @@
 public class MazeSolver {
     private Maze board;
+    private boolean animate;
 
-    public void solve() {
-	if (args == 1) {
+    public MazeSolver(String filename) {
+	this(filename, false);
+    }
+
+    public MazeSolver(String filename, boolean animate) {
+	this.animate = animate;
+	board = new Maze(filename);
+    }
+
+    public String toString() {
+	// etc
+    }
+	
+
+    public void solve(int style) {
+	if (style == 0) { // DFS
+	    Frontier front = new FrontierStack();
 	}
+	else if (style == 1) { //  BFS
+	    Fronter front = new FrontierQueue();
+	}
+	else if (style == 2 || style == ) { // BestFirst or A*
+	    Frontier front = new FrontierPriorityQueue();
+	}
+	// throw exception if style >= 4??
+	front.add(maze.getStart());
     }
 }
